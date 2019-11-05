582898e878016d8f63c69233b2b8d1bc4faa0963
Mohamed Samy <muhamed.samy1@gmail.com>
2019-11-05 20:54:58 +0100

    Start the phone activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,7 +43,8 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     public void createPhoneIntent(View view){
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-        phoneIntent.setData(Uri.parse("tel:01789362933"));
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
+
     }
 }
