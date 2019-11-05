75726a1fffcaf1196a293e406a31d19a81e78204
Mohamed Samy <muhamed.samy1@gmail.com>
2019-11-05 20:17:31 +0100

    Start Google maps activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -26,6 +28,15 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        Uri uriAdress = Uri.parse("geo:0,0?q=16 E Veilchengasse Deggendorf, 94469");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAdress);
+        mapIntent.setPackage("com.android.apps.maps");
+        //check if there is a component to handle this intent
+        if(mapIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
